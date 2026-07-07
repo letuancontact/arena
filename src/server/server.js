8fff3a675c192257ea8ac1ad7b164bce9796b40e
letuancontact <letuan.contact@gmail.com>
2026-07-07 11:39:54 +0700

    Update server.js

--- src/server/server.js
+++ src/server/server.js
@@ -320,6 +320,8 @@ setInterval(() => {
   }
 }, 16);
 
-server.listen(3000, () =>
-  console.log("Server running on http://localhost:3000")
+// DÁN VÀO ĐOẠN NÀY:
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
 );
