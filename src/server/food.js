e49bc9e17e4877b14e5223fadc9fd30a8f499f15
letuancontact <letuan.contact@gmail.com>
2026-07-08 20:54:09 +0700

    Update food.js

--- src/server/food.js
+++ src/server/food.js
@@ -1,21 +1,24 @@
-const {
-  FOOD_TYPES,
-  FOOD_COUNT,
-  MAP_WIDTH,
-  MAP_HEIGHT,
-} = require("./constants");
+"use strict";
+
+const { FOOD_TYPES, FOOD_COUNT, MAP_WIDTH, MAP_HEIGHT } = require("../shared/constants");
 const { uuid } = require("./utils");
+
 function spawnFood(foodList) {
+  const addedFood = []; // Mảng chứa thức ăn mới sinh ra
   while (foodList.length < FOOD_COUNT) {
     const foodType = FOOD_TYPES[Math.floor(Math.random() * FOOD_TYPES.length)];
-    foodList.push({
+    const newFood = {
       id: uuid(),
       x: Math.random() * MAP_WIDTH,
       y: Math.random() * MAP_HEIGHT,
       radius: foodType.radius,
       xp: foodType.xp,
       type: foodType.type,
-    });
+    };
+    foodList.push(newFood);
+    addedFood.push(newFood);
   }
+  return addedFood;
 }
+
 module.exports = { spawnFood };
