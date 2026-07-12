f745a0d488bca155157c59f1840f5f8281eb2414
letuancontact <letuan.contact@gmail.com>
2026-07-13 01:56:43 +0700

    Update constants.js

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,9 +1,5 @@
 "use strict";
 
-/**
- * @fileoverview Cấu hình hệ thống dùng chung cho cả Client và Server.
- */
-
 const GAME_CONFIG = {
   // Bản đồ & Số lượng
   MAP_WIDTH: 3000,
@@ -17,12 +13,12 @@ const GAME_CONFIG = {
   MIN_SPEED: 2.2,
   MAX_SPEED: 4,
   SPRINT_MULTIPLIER: 2,
-  BOT_SPEED_MULTIPLIER: 0.70,  // BOT GIỜ CHỈ CHẠY BẰNG 70% NGƯỜI CHƠI
+  BOT_SPEED_MULTIPLIER: 0.55,  // BOT GIỜ CHỈ CHẠY BẰNG 55% NGƯỜI CHƠI CHÍNH (Cực chậm)
   MIN_VELOCITY: 0.1,
   SMOOTHING_FACTOR: 0.15,
   ANGLE_LERP: 0.25,
 
-  // Thông số Mạng (Network & Tickrate)
+  // Thông số Mạng
   SERVER_TICK_RATE: 16,
   SERVER_BROADCAST_RATE: 50,
   HEAVY_TICK_RATE: 200,
@@ -37,25 +33,17 @@ const GAME_CONFIG = {
   HIT_COOLDOWN: 5000,          
   RESPAWN_TIME: 5000,
 
-  // Kinh nghiệm (XP) & Thức ăn
+  // Kinh nghiệm (XP)
   XP_LOSS_PERCENT: 0.025,
   XP_LOSS_INTERVAL: 200,
   KILL_SCORE_MULTIPLIER_ATTACKER: 0.3,
   KILL_SCORE_MULTIPLIER_HUD: 0.6,
   
   FOOD_TYPES: [
-    { radius: 8, xp: 5, type: 0 },
-    { radius: 8, xp: 6, type: 1 },
-    { radius: 8, xp: 7, type: 2 },
-    { radius: 12, xp: 10, type: 3 },
-    { radius: 12, xp: 12, type: 4 },
-    { radius: 12, xp: 14, type: 5 },
-    { radius: 16, xp: 18, type: 6 },
-    { radius: 16, xp: 22, type: 7 },
-    { radius: 16, xp: 26, type: 8 },
-    { radius: 18, xp: 30, type: 9 },
-    { radius: 20, xp: 34, type: 10 },
-    { radius: 22, xp: 38, type: 11 },
+    { radius: 8, xp: 5, type: 0 }, { radius: 8, xp: 6, type: 1 }, { radius: 8, xp: 7, type: 2 },
+    { radius: 12, xp: 10, type: 3 }, { radius: 12, xp: 12, type: 4 }, { radius: 12, xp: 14, type: 5 },
+    { radius: 16, xp: 18, type: 6 }, { radius: 16, xp: 22, type: 7 }, { radius: 16, xp: 26, type: 8 },
+    { radius: 18, xp: 30, type: 9 }, { radius: 20, xp: 34, type: 10 }, { radius: 22, xp: 38, type: 11 },
   ],
 
   RADIUS_TABLE: [
